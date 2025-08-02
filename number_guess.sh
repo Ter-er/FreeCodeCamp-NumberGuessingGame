d32b07798766c321cfba79133f3238b0bc37d6ab
Ter-er <tfgbem@gmail.com>
2025-08-02 17:57:11 -0400

    feat: random number done

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+# Number Guessing Game
+
+RAND_NUM=$(( (RANDOM % 1000) + 1 ))
+
+while [[ $GUESS -ne $RAND_NUM ]]
+do
+  read GUESS
+  if [[ $GUESS -lt $RAND_NUM ]]
+  then
+    echo Higher
+  elif [[ $GUESS -gt $RAND_NUM ]]
+  then
+    echo Lower
+  elif [[ $GUESS -eq $RAND_NUM ]]
+  then
+    echo Correct
+  fi
+done
